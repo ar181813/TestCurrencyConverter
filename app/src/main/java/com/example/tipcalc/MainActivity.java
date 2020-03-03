7295238862a9032457d5059bbdbd446b56572354
a828719r <ar181813@scots.edinboro.edu>
2020-03-03 12:12:46 -0500

    initial commit

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -9,17 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView resultTextBox;
     Button calcButton;
-    EditText weightNum, timeNum;
+    EditText dollarNum;
     Spinner spinOps;
-    String[] activities={"Aerobics","Bicycling","Dancing","Running","Swimming", "Walking"};
-    String activitySelected;
-    double activity;
-    double kgInLbs = 2.2;
-    int minutesInHour = 60;
+    String[] currencies={"Euro","Yuan","Pounds","Rupies","Yen"};
+    String currencySelected;
+    double currency;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,52 +31,45 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         calcButton.setText("Display result");
         calcButton.setOnClickListener(myClickListener);
 
-        weightNum = findViewById(R.id.amount_weight);
-        timeNum = findViewById(R.id.amount_time);
+        dollarNum = findViewById(R.id.amount_Dollars);
+        dollarNum.setText("");
 
-        timeNum.setText("");
-        weightNum.setText("");
-
-        /* Spinner list of activities */
+        /* Spinner list of currencies */
         spinOps = findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, activities);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, currencies);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinOps.setAdapter(adapter);
         spinOps.setOnItemSelectedListener(this);
-
     }
 
     private View.OnClickListener myClickListener = new View.OnClickListener()
     {
         public void onClick(View v) {
-            EditText etWeight = (EditText) findViewById(R.id.amount_weight);
-            EditText etTime = (EditText) findViewById(R.id.amount_time);
+            EditText etDollars = findViewById(R.id.amount_Dollars);
+            String strDollars = etDollars.getText().toString();
 
-            String strWeight = etWeight.getText().toString();
-            String strTime = etTime.getText().toString();
+            double mets[] = {0.93,7.02,0.78,71.88,112.07};
+            //euro, yuan, pounds, rupies, yen
 
-            double mets[] = {7.3,7.5,5,9.8,6,4.3};
-            //aerobics, bicycling, dancing, running, swimming, walking
-
-            if (activitySelected=="Aerobics"){activity=mets[0]; resultTextBox.setText(Double.toString(activity));}
-            else if (activitySelected=="Bicycling"){activity=mets[1]; resultTextBox.setText(Double.toString(activity));}
-            else if (activitySelected=="Dancing"){activity=mets[2]; resultTextBox.setText(Double.toString(activity));}
-            else if (activitySelected=="Running"){activity=mets[3]; resultTextBox.setText(Double.toString(activity));}
-            else if (activitySelected=="Swimming"){activity=mets[4]; resultTextBox.setText(Double.toString(activity));}
-            else if (activitySelected=="Walking"){activity=mets[5]; resultTextBox.setText(Double.toString(activity));}
+            if (currencySelected=="Euro"){currency=mets[0]; resultTextBox.setText(Double.toString(currency));}
+            else if (currencySelected=="Yuan"){currency=mets[1]; resultTextBox.setText(Double.toString(currency));}
+            else if (currencySelected=="Pounds"){currency=mets[2]; resultTextBox.setText(Double.toString(currency));}
+            else if (currencySelected=="Rupies"){currency=mets[3]; resultTextBox.setText(Double.toString(currency));}
+            else if (currencySelected=="Yen"){currency=mets[4]; resultTextBox.setText(Double.toString(currency));}
             else {resultTextBox.setText("Please select the operation");}
 
-            Double dblWt = Double.parseDouble(strWeight) / (kgInLbs);
-            Double dblTime = Double.parseDouble(strTime) / (minutesInHour);
-            Double calc = dblWt * activity * dblTime;
+            Double dblDollars = Double.parseDouble(strDollars);
+            Double calc = dblDollars * currency;
+            DecimalFormat df = new DecimalFormat("0.00");
 
-            resultTextBox.setText(String.format("%.0f", calc));
+            resultTextBox.setText(df.format(calc) + " " + currencySelected);
+           // resultTextBox.setText(String.format("%.0f", calc));
         }
     };
 
     public void onItemSelected (AdapterView<?> p,View v,int position,long id) {
-        activitySelected=activities[position];
-        resultTextBox.setText("You have selected " + activitySelected);
+        currencySelected=currencies[position];
+        resultTextBox.setText("You have selected " + currencySelected);
     }
     public void onNothingSelected(AdapterView<?> p) {
         resultTextBox.setText("Please select ops");
